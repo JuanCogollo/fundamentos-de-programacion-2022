1e977c345a4e3780b0d8da8a801997397c103ba6
JuanCogollo <109865594+JuanCogollo@users.noreply.github.com>
2022-11-05 11:26:22 -0500

    Add files via upload

---
+++ modulo10/ejercicio1.py
@@ -0,0 +1,52 @@
+def str_to_num(arr, idx):
+    new_arr = []
+    for i in range(len(arr)):
+        arr[i][idx] = int(arr[i][idx])
+        new_arr.append(arr[i])
+
+    return new_arr
+
+
+def print_format(arr):
+    for i in arr:
+        print(f'{i[0]} {i[1].upper()} {i[2]}')
+
+
+def ordering(arr):
+    arr_cleaned = []
+    arr = str_to_num(arr, 3)
+    arr = str_to_num(arr, 1)
+    for i in range(len(arr)):
+        if arr[i][2] == 'positiva':
+            arr_cleaned.append(arr[i])
+        else:
+            continue
+
+    arr_cleaned.sort(key=lambda x: x[3], reverse=True)
+
+    for i in range(1, len(arr_cleaned)):
+        f = arr_cleaned[i - 1]
+        if f[3] == arr_cleaned[i][3] and f[1] < arr_cleaned[i][1]:
+            arr_cleaned[i - 1], arr_cleaned[i] = arr_cleaned[i], arr_cleaned[i - 1]
+        else:
+            continue
+
+    for i in range(len(arr_cleaned)):
+        arr_cleaned[i].pop(0)
+
+    return arr_cleaned
+
+
+def main():
+    data = []
+
+    ciudadanos = int(input())
+
+    for i in range(ciudadanos):
+        ciudadano = input()
+        data.append(ciudadano.split(" "))
+
+    print_format(ordering(data))
+
+
+main()
